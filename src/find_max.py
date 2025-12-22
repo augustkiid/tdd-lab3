97d1fa612a18eb2414c3420cdc3e08d720df5b1c
augustkiid <erretamv@gmail.com>
2025-12-22 10:57:22 +0400

    Create find_max.py

---
+++ src/find_max.py
@@ -0,0 +1,7 @@
+def find_max(numbers):
+    max_value = numbers[0]
+    if numbers[1] > max_value:
+        max_value = numbers[1]
+    if len(numbers) > 2 and numbers[2] > max_value:
+        max_value = numbers[2]
+    return max_value
